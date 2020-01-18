f484e4f3a87b0761cd9064bb02e42966e816da03
vitaliyhtml5 <58391292+vitaliyhtml5@users.noreply.github.com>
2020-01-18 21:08:39 +0200

    html, css, js - 100% production ready

--- html/script.js
+++ html/script.js
@@ -1,75 +1,51 @@
 //TABS
 function changeTabs() {	
 	let tabs = {
-		createTable: document.getElementById('create-table'),
-		selectTable: document.getElementById('choose-table'),
-		changeTable: document.getElementById('change-table'),
-		joinTable: document.getElementById('join-table'),
-		tasks: document.getElementById('tasks'),
-		showHideTabs(show, hideFirst, hideSecond, hideThird, hideForth) {
+		htmlTable: document.getElementById('html-table'),
+		cssTable: document.getElementById('css-table'),
+		filesTable: document.getElementById('files-table'),
+		showHideTabs(show, hideFirst, hideSecond) {
 			show.style.display = 'block';
 			hideFirst.style.display = 'none';
 			hideSecond.style.display = 'none';
-			hideThird.style.display = 'none';
-			hideForth.style.display = 'none';
 		},
 	};
 	let sidebar = {
-		createSide: document.querySelector('.sidebar__item_create'),
-		selectSide: document.querySelector('.sidebar__item_choose'),
-		changeSide: document.querySelector('.sidebar__item_change'),
-		joinSide: document.querySelector('.sidebar__item_join'),
-		tasksSide: document.querySelector('.sidebar__item_tasks'),
-		changeSidebar(selected, unselectedFirst, unselectedSecond, unselectedThird, unselectedForth) {
+		htmlSide: document.querySelector('.sidebar__item_html'),
+		cssSide: document.querySelector('.sidebar__item_css'),
+		filesSide: document.querySelector('.sidebar__item_files'),
+		changeSidebar(selected, unselectedFirst, unselectedSecond) {
 			selected.classList.add('sidebar__item_selected');
 			unselectedFirst.classList.remove('sidebar__item_selected');
 			unselectedSecond.classList.remove('sidebar__item_selected');
-			unselectedThird.classList.remove('sidebar__item_selected');
-			unselectedForth.classList.remove('sidebar__item_selected');
 			unselectedFirst.classList.add('sidebar__item');
 			unselectedSecond.classList.add('sidebar__item');
-			unselectedThird.classList.add('sidebar__item');
-			unselectedForth.classList.add('sidebar__item');
 		},
 	};
 	let links = {
-		createLink: document.querySelector('.tutorial-create'),
-		selectLink: document.querySelector('.tutorial-select'),
-		changeLink: document.querySelector('.tutorial-change'),
-		joinLink: document.querySelector('.tutorial-join'),
-		tasksLink: document.querySelector('.tutorial-task'),
-		showHideLinks(show, hideFirst, hideSecond, hideThird, hideForth) {
+		htmlLink: document.querySelector('.tutorial-html'),
+		cssLink: document.querySelector('.tutorial-css'),
+		filesLink: document.querySelector('.tutorial-files'),
+		showHideLinks(show, hideFirst, hideSecond) {
 			show.style.display = 'block';
 			hideFirst.style.display = 'none';
 			hideSecond.style.display = 'none';
-			hideThird.style.display = 'none';
-			hideForth.style.display = 'none';
 		},
 	};
-	sidebar.createSide.onclick = function() {
-		tabs.showHideTabs(tabs.createTable, tabs.selectTable, tabs.changeTable, tabs.joinTable, tabs.tasks);
-		sidebar.changeSidebar(sidebar.createSide, sidebar.selectSide, sidebar.changeSide, sidebar.joinSide, sidebar.tasksSide);
-		links.showHideLinks(links.createLink, links.selectLink, links.changeLink, links.joinLink, links.tasksLink);
+	sidebar.htmlSide.onclick = function() {
+		tabs.showHideTabs(tabs.htmlTable, tabs.cssTable, tabs.filesTable);
+		sidebar.changeSidebar(sidebar.htmlSide, sidebar.cssSide, sidebar.filesSide);
+		links.showHideLinks(links.htmlLink, links.cssLink, links.filesLink);
 	}
-	sidebar.selectSide.onclick = function() {
-		tabs.showHideTabs(tabs.selectTable, tabs.createTable, tabs.changeTable, tabs.joinTable, tabs.tasks);
-		sidebar.changeSidebar(sidebar.selectSide, sidebar.createSide, sidebar.changeSide, sidebar.joinSide, sidebar.tasksSide);
-		links.showHideLinks(links.selectLink, links.createLink, links.changeLink, links.joinLink, links.tasksLink);
+	sidebar.cssSide.onclick = function() {
+		tabs.showHideTabs(tabs.cssTable, tabs.htmlTable, tabs.filesTable);
+		sidebar.changeSidebar(sidebar.cssSide, sidebar.htmlSide, sidebar.filesSide);
+		links.showHideLinks(links.cssLink, links.htmlLink, links.filesLink);
 	}
-	sidebar.changeSide.onclick = function() {
-		tabs.showHideTabs(tabs.changeTable, tabs.createTable, tabs.selectTable, tabs.joinTable, tabs.tasks);
-		sidebar.changeSidebar(sidebar.changeSide, sidebar.selectSide, sidebar.createSide, sidebar.joinSide, sidebar.tasksSide);
-		links.showHideLinks(links.changeLink, links.createLink, links.selectLink, links.joinLink, links.tasksLink);
-	}
-	sidebar.joinSide.onclick = function() {
-		tabs.showHideTabs(tabs.joinTable, tabs.createTable, tabs.selectTable, tabs.changeTable, tabs.tasks);
-		sidebar.changeSidebar(sidebar.joinSide, sidebar.selectSide, sidebar.createSide, sidebar.changeSide, sidebar.tasksSide);
-		links.showHideLinks(links.joinLink, links.createLink, links.selectLink, links.changeLink, links.tasksLink);
-	}
-	sidebar.tasksSide.onclick = function() {
-		tabs.showHideTabs(tabs.tasks, tabs.createTable, tabs.selectTable, tabs.changeTable, tabs.joinTable);
-		sidebar.changeSidebar(sidebar.tasksSide, sidebar.selectSide, sidebar.createSide, sidebar.changeSide, sidebar.joinSide);
-		links.showHideLinks(links.tasksLink, links.createLink, links.selectLink, links.changeLink, links.joinLink);
+	sidebar.filesSide.onclick = function() {
+		tabs.showHideTabs(tabs.filesTable, tabs.htmlTable, tabs.cssTable);
+		sidebar.changeSidebar(sidebar.filesSide, sidebar.htmlSide, sidebar.cssSide);
+		links.showHideLinks(links.filesLink, links.htmlLink, links.cssLink);
 	}
 }
 
@@ -118,58 +94,18 @@ function openCloseBurger() {
 	};
 }
 
-//TASKS
-function showHideAnsw() {
-	let btnTasks = [
-		document.querySelector('.task-answer_first'),
-		document.querySelector('.task-answer_second'),
-		document.querySelector('.task-answer_third'),
-		document.querySelector('.task-answer_forth'),
-		document.querySelector('.task-answer_fifth'),
-		document.querySelector('.task-answer_sixth'),
-	];
-	let answTasks = [
-		document.querySelector('.show-answer_first'),
-		document.querySelector('.show-answer_second'),
-		document.querySelector('.show-answer_third'),
-		document.querySelector('.show-answer_forth'),
-		document.querySelector('.show-answer_fifth'),
-		document.querySelector('.show-answer_sixth'),
-	];
-	
-	let arrow = document.querySelectorAll('.arrow-down');
-	let toggleAnsw = answ => {
-		answ.classList.toggle('show-answer');
-		for (let i=0; i<arrow.length; i++) {
-		}
-	}
+// FILES
+function showHideFiles() {
+	let btnFile = document.querySelector('.files-text');
+	let allFile = document.querySelector('.files-all');
+	let arrowDown = document.querySelector('.arrow-down');
 
-	btnTasks[0].onclick = function () {
-		toggleAnsw(answTasks[0]);
-		arrow[0].classList.toggle('arrow-up');
-	}
-	btnTasks[1].onclick = function () {
-		toggleAnsw(answTasks[1]);
-		arrow[1].classList.toggle('arrow-up');
-	}
-	btnTasks[2].onclick = function () {
-		toggleAnsw(answTasks[2]);
-		arrow[2].classList.toggle('arrow-up');
-	}
-	btnTasks[3].onclick = function () {
-		toggleAnsw(answTasks[3]);
-		arrow[3].classList.toggle('arrow-up');
-	}
-	btnTasks[4].onclick = function () {
-		toggleAnsw(answTasks[4]);
-		arrow[4].classList.toggle('arrow-up');
-	}
-	btnTasks[5].onclick = function () {
-		toggleAnsw(answTasks[5]);
-		arrow[5].classList.toggle('arrow-up');
+	btnFile.onclick = function () {
+		allFile.classList.toggle('show-file');
+		arrowDown.classList.toggle('arrow-up');
 	}
 }
 
 changeTabs();
 openCloseBurger();
-showHideAnsw()
+showHideFiles();

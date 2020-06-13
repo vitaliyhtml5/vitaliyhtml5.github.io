8e329f1a267aec8fa9b58811a4f8c233142d8e8a
vitaliyhtml5 <58391292+vitaliyhtml5@users.noreply.github.com>
2020-06-13 19:11:42 +0300

    100% - production ready

--- america/script.js
+++ america/script.js
@@ -5,6 +5,7 @@ function openOverlay() {
 
 	for(let i = 0; i < modalUpdate.length; i++) {
 		modalUpdate[i].onclick = () => {
+			event.preventDefault();
 			overlay.style.display = 'block';
 		}
 	}
@@ -15,3 +16,26 @@ document.querySelector('.modal__btn').onclick = () => {
 }
 
 openOverlay();
+
+
+let dateTour = document.querySelectorAll('.table-date');
+document.querySelector('.pagination-btn_first').onclick = changePaginationFirst;
+document.querySelector('.pagination-btn_second').onclick = changePaginationSecond;
+
+function changePaginationFirst() {
+	dateTour[0].innerText = '15 октября';
+	dateTour[1].innerText = '20 ноября';
+	dateTour[2].innerText = '24 декабря';
+}
+
+function changePaginationSecond() {
+	dateTour[0].innerText = '30 января';
+	dateTour[1].innerText = '12 февраля';
+	dateTour[2].innerText = '7 марта';
+}
+
+
+
+
+
+

34ab702aa189924113d6be61913c9eaf96c86ddd
vitaliyhtml5 <vitaliyhtml5@gmail.com>
2022-03-26 14:07:08 +0200

    remove find friend app

--- find_friend/cypress/cypress/support/edit_friend.js
+++
@@ -1,8 +0,0 @@
-Cypress.Commands.add('editFriend', (random,name,age,hobby) => {
-    cy.get('.change-name-friend').click({force: true});
-    cy.get('.name-choose').eq(random).click({force: true});
-    cy.get('.change_friend input').eq(0).clear({force: true}).type(name);
-    cy.get('.change_friend input').eq(1).clear({force: true}).type(age);
-    cy.get('.change_friend input').eq(2).clear({force: true}).type(hobby);
-    cy.contains('.change_friend button','Confirm').click();
-});
